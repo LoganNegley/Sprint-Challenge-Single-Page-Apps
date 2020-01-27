0167dfc15c798fc8bfe8566b909006bf152c0838
Logan Negley <logannegley@gmail.com>
2020-01-26 19:06:23 -0600

    Card info styled with styled components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,25 +3,41 @@ import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button,
 } from 'reactstrap';
+import styled from 'styled-components';
+
+const CardDiv = styled.div`
+width:50%;
+`;
+
+const Header = styled.h2`
+text-decoration: underline;
+`;
+
+const SubTitle = styled.p`
+font-size: 20px;
+`;
+
+const Span = styled.span`
+font-weight:bold;
+`;
 
 export default function CharacterCard(props) {
  console.log(props.character)
 
   return(
-
     <div className='character-wrapper'>
-      <Card>
-        <CardImg src={`${props.character.image}`} alt={`Picture of ${props.character.name}`} />
+      <CardDiv>
+        <CardImg width='100px' src={`${props.character.image}`} alt={`Picture of ${props.character.name}`} />
         <CardBody>
-          <CardTitle><h2>{props.character.name}</h2></CardTitle>
-          <CardSubtitle>Info about {props.character.name}</CardSubtitle>
+          <CardTitle><Header>{props.character.name}</Header></CardTitle>
+          <CardSubtitle><SubTitle>Info about {props.character.name}</SubTitle></CardSubtitle>
           <CardText>
-            <p>Status: {props.character.status}</p>
-            <p>Species: {props.character.species}</p>
-            <p>Gender: {props.character.gender}</p>
+            <p><Span>Status:</Span> {props.character.status}</p>
+            <p><Span>Species: </Span>{props.character.species}</p>
+            <p><Span>Gender:</Span> {props.character.gender}</p>
           </CardText>
         </CardBody>
-      </Card>
+      </CardDiv>
     </div>
   );
 }
