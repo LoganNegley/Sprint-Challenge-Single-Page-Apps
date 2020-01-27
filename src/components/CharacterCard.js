7366045d11ed579fef424145edb8f99d49c56ce5
Logan Negley <logannegley@gmail.com>
2020-01-26 18:25:16 -0600

    imported reactstraps to CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button,
+} from 'reactstrap';
 
 export default function CharacterCard(props) {
  console.log(props.character)
@@ -6,10 +10,9 @@ export default function CharacterCard(props) {
   return(
     <div className='character-card'>
       <h2>{props.character.name}</h2>
-      <p>{props.character.status}</p>
-      <p>{props.character.species}</p>
-
-      <p>{props.character.gender}</p>
+      <p>Status: {props.character.status}</p>
+      <p>Species: {props.character.species}</p>
+      <p>Gender: {props.character.gender}</p>
 
     </div>
   );
