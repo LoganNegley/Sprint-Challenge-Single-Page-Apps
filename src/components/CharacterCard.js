740e6f5365217df99898229bb11e5956168725b3
Logan Negley <logannegley@gmail.com>
2020-01-26 18:20:18 -0600

    added character info

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,6 +6,11 @@ export default function CharacterCard(props) {
   return(
     <div className='character-card'>
       <h2>{props.character.name}</h2>
+      <p>{props.character.status}</p>
+      <p>{props.character.species}</p>
+
+      <p>{props.character.gender}</p>
+
     </div>
   );
 }
